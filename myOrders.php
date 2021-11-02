8619f1c698691f8730d65d426b4ed6d1029022e4
ddfung <danielfung.yh@gmail.com>
2021-11-03 01:11:00 +0800

    Adds null order_id error catching

--- myOrders.php
+++ myOrders.php
@@ -25,7 +25,7 @@
   $result = $db -> query($query); //query submission
   // print_r($result);
   // echo "{$result}";
-  $num_results = $result->num_rows; //retrieve num of rows in result set    
+  $num_results = $result->num_rows; //retrieve num of rows in result set   
   $item_names = array();
   $item_size = array();
   $item_quantity = array();
@@ -80,11 +80,11 @@
         <h2>MY ORDERS</h2>
         <form action="myOrders.php" method="POST">
             <div align='center'id="order_tag"><label></i> Order ID  </label>
-            <input type="number" id="order_id" step="1" min="0" name="order_id" placeholder="Enter Order_id"></div><br><br>
+            <input type="number" id="order_id" step="1" min="0" name="order_id" placeholder="Enter Order id"></div><br><br>
             <div class='submitinfoBtn' align='center'><input type="submit" id="submitinfoBtn" name="submit" value="Submit" /></div>
         </form><br><br><br>
         <?php 
-          if ($order_id) {
+          if ($num_results) {
             echo "<table class='itemsorderedTable'>
               <tr>
                 <th style='font-size:x-large;background-color:#e8e8e8' colspan='4'>Items Ordered</th>
@@ -116,6 +116,9 @@
                 <td colspan='2'style='font-size: 18px'><b>Estimated delivery:</b> {$deliveryDate}</td>
                 </tr>        
             </table>";
+          } 
+          elseif ($num_results == '0') {
+            echo "<div align='center' style='font-size: 18px;'>Sorry, Order ID not found in our database. Please try again.</div>";
           }
         ?>
           </main>
